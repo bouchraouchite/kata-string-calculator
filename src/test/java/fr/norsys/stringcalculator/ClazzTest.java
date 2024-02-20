89071b8a3b29940be51677b19dcc9f4f1e6e37dc
bouchraouchite <bouchra.ouchite@e-polytechnique.ma>
2024-02-20 13:37:59 +0100

    Tests

--- src/test/java/fr/norsys/stringcalculator/ClazzTest.java
+++ src/test/java/fr/norsys/stringcalculator/ClazzTest.java
@@ -1,64 +1,50 @@
 package fr.norsys.stringcalculator;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClazzTest {
 
-    private StringCalculator calculator;
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Before
-    public void initialize() {
-        calculator = new StringCalculator();
+    @Test
+    public void testAddEmptyString() {
+        Clazz clazz = new Clazz();
+        int result = clazz.add("");
+        assertEquals(0, result);
     }
 
     @Test
-    public void emptyStringShouldReturn0() {
-        assertEquals(calculator.add(""), 0);
+    public void testAddSingleNumber() {
+        Clazz clazz = new Clazz();
+        int result = clazz.add("5");
+        assertEquals(5, result);
     }
 
     @Test
-    public void numberStringShouldReturnSameNumber() {
-        assertEquals(calculator.add("1"), 1);
-        assertEquals(calculator.add("5"), 5);
+    public void testAddTwoNumbers() {
+        Clazz clazz = new Clazz();
+        int result = clazz.add("5,3");
+        assertEquals(8, result);
     }
 
     @Test
-    public void numbersCommaDelimitedShouldBeSummed() {
-        assertEquals(calculator.add("1,2"), 3);
-        assertEquals(25, calculator.add("10,15"));
+    public void testAddNewLineDelimiter() {
+        Clazz clazz = new Clazz();
+        int result = clazz.add("1\n2,3");
+        assertEquals(6, result);
     }
 
     @Test
-    public void numbersNewlineDelimitedShouldBeSummed() {
-        assertEquals(calculator.add("1\n2"), 3);
-        assertEquals(calculator.add("11\n13"), 24);
+    public void testAddIgnoreNumbersGreaterThan1000() {
+        Clazz clazz = new Clazz();
+        int result = clazz.add("2,1001");
+        assertEquals(2, result);
     }
 
     @Test
-    public void threeNumbersDelimitedAnywayShouldBeSummed() {
-        assertEquals(calculator.add("1,2,3"), 6);
-        assertEquals(calculator.add("5\n2\n3"), 10);
-    }
-
-    @Test
-    public void negativeInputReturnsException() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Negative input!");
-        calculator.add("-1");
-        calculator.add("-5,10\n-15");
-    }
-
-    @Test
-    public void numbersGreaterThan1000AreIgnored() {
-        assertEquals(calculator.add("5,12,1001"), 17);
-        assertEquals(calculator.add("14124,22\n4,1214"), 26);
+    public void testAddNegativeNumber() {
+        Clazz clazz = new Clazz();
+        assertThrows(IllegalArgumentException.class, () -> {
+            clazz.add("-2,3");
+        });
     }
 }
